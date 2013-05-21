e6f4ab7e284763e0fa1125f7c383acd749a9d87c
SPolischook <spolischook@gmail.com>
2013-05-21 08:44:36 +0300

    Add TypeEstateAdmin

---
+++ src/Spolischook/RealEstateBundle/Admin/TypeEstateAdmin.php
@@ -0,0 +1,32 @@
+<?php
+namespace Spolischook\RealEstateBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class TypeEstateAdmin extends Admin
+{
+    public function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+        ;
+    }
+
+    public function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name', null, array('label' => 'sonata.name'))
+        ;
+    }
+
+    public function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+        ;
+    }
+}
