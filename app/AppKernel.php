adbe7ef72504711ec7a1675296b663c2413ebc47
SPolischook <spolischook@gmail.com>
2013-05-30 13:40:19 +0300

    Enable liip image bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
             new Spolischook\RealEstateBundle\RealEstateBundle(),
             new Spolischook\UserBundle\UserBundle(),
             new Spolischook\MediaBundle\MediaBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
